e5d2e53dbd21bc3a51fda3fc7333d232862ca7e9
Teodoro Calle Lara <teodoro.calle@mergedev.co>
2023-10-10 16:16:30 -0500

    Initial Commit

---
+++ InciosSwiftUI/InciosSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InciosSwiftUIApp.swift
+//  InciosSwiftUI
+//
+//  Created by ADMIN on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct InciosSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
